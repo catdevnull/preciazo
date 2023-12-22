5e55ad7131db3afd794e13d3519d617ed60f0338
Nulo <git@nulo.in>
2023-12-22 15:45:20 -0300

    agregar stock al schema

--- scraper/db/schema.ts
+++ scraper/db/schema.ts
@@ -1,9 +1,10 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text, b } from "drizzle-orm/sqlite-core";
 
 export const precios = sqliteTable("precios", {
   id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-  fetchedAt: integer("fetched_at", { mode: "timestamp" }).notNull(),
-  precioCentavos: integer("precio_centavos").notNull(),
   ean: text("ean").notNull(),
+  fetchedAt: integer("fetched_at", { mode: "timestamp" }).notNull(),
+  precioCentavos: integer("precio_centavos"),
+  inStock: integer("in_stock", { mode: "boolean" }),
   url: text("url"),
 });
