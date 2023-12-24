08f62c78dbed414b13b8528794eadf01dce0f5fc
Nulo <git@nulo.in>
2023-12-24 18:12:06 -0300

    usar bun para db-datos

--- db-datos/migrate.ts
+++ db-datos/migrate.ts
@@ -1,11 +1,11 @@
-import { migrate } from "drizzle-orm/better-sqlite3/migrator";
-import Database from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3";
+import Database from "bun:sqlite";
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 import * as schema from "./schema.js";
 
 const sqlite = new Database("../scraper/sqlite.db");
 const db = drizzle(sqlite, { schema });
 
-await migrate(db, { migrationsFolder: "./drizzle" });
+migrate(db, { migrationsFolder: "./drizzle" });
 
 sqlite.close();
