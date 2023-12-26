bf0313a4b6d98ceb943bb0482a5cfb76c92b2e3d
Nulo <git@nulo.in>
2023-12-26 17:44:12 -0300

    añadir productos de ejemplo en index

--- sitio/src/routes/+page.svelte
+++ sitio/src/routes/+page.svelte
@@ -6,12 +6,37 @@
 
 <h1 class="text-xl">WIP</h1>
 
-<ul>
-  {#each data.precios as product}
+<section>
+  <h2 class="text-lg font-bold">Ejemplos</h2>
+  <ul>
     <li>
-      <a href={`/ean/${product.ean}`}>
-        {product.name}
+      <a href="/ean/7790070410795">
+        Cookies Sabor Vainilla Con Chips De Chocolate Exquisita Paq 300 Grm
       </a>
     </li>
-  {/each}
-</ul>
+    <li>
+      <a href="/ean/7794000006911"
+        >Sopa Instantánea KNORR QUICK Zapallo Romero Sobres 5 Un.</a
+      >
+    </li>
+    <li>
+      <a href="/ean/7798062540253">Agua Saborizada Levité Pera 1,5 Lts.</a>
+    </li>
+    <li>
+      <a href="/ean/7790895000430">Gaseosa Coca-Cola Sabor Original 1,5 Lts.</a>
+    </li>
+  </ul>
+</section>
+
+<section>
+  <h2 class="text-lg font-bold">Random</h2>
+  <ul>
+    {#each data.precios as product}
+      <li>
+        <a href={`/ean/${product.ean}`}>
+          {product.name}
+        </a>
+      </li>
+    {/each}
+  </ul>
+</section>
