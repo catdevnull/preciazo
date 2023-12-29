bc95d9c2556da71e587cce1ec50e50d290d61413
Nulo <git@nulo.in>
2023-12-29 19:54:30 -0300

    migrar en sitio

--- db-datos/migrate.ts
+++ db-datos/migrate.ts
@@ -1,15 +1,16 @@
 import Database from "bun:sqlite";
-import { join } from "node:path";
+import { join, dirname } from "node:path";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 import * as schema from "./schema.js";
 import { DB_PATH } from "./drizzle.config.js";
 
+const url = new URL(import.meta.url);
 export function migrateDb() {
   const sqlite = new Database(DB_PATH);
   const db = drizzle(sqlite, { schema });
 
-  migrate(db, { migrationsFolder: join(import.meta.dir, "drizzle") });
+  migrate(db, { migrationsFolder: join(dirname(url.pathname), "drizzle") });
   sqlite.run(`
 pragma journal_mode = WAL;
 PRAGMA synchronous = NORMAL;
--- sitio/src/lib/server/db.ts
+++ sitio/src/lib/server/db.ts
@@ -1,7 +1,10 @@
 import Database from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import * as schema from "db-datos/schema.js";
+import { migrateDb } from "db-datos/migrate.js";
 import { env } from "$env/dynamic/private";
+
+migrateDb();
 
 const sqlite = new Database(env.DB_PATH ?? "../scraper/sqlite.db");
 
