cd6bbbdbe80821afd3f555e9e7fbb6ba8625bb19
Nulo <86896666+catdevnull@users.noreply.github.com>
2023-12-31 15:52:16 +0000

    scrapear urls por separado

--- scraper/cli.ts
+++ scraper/cli.ts
@@ -1,8 +1,17 @@
+import { scrapCarrefourProducts } from "../carrefour-link-scraper/index.js";
+import { scrapCotoProducts } from "../coto-link-scraper/index.js";
+import { scrapDiaProducts } from "../dia-link-scraper/index.js";
 import { auto } from "./auto.js";
 import { parseWarc } from "./scrap.js";
 
 if (process.argv[2] === "auto") {
   await auto();
+} else if (process.argv[2] === "scrap-carrefour-links") {
+  await scrapCarrefourProducts()
+} else if (process.argv[2] === "scrap-dia-links") {
+  await scrapDiaProducts()
+} else if (process.argv[2] === "scrap-coto-links") {
+  await scrapCotoProducts()
 } else if (process.argv[2] === "scrap") {
   const warcPaths = process.argv.slice(3);
   if (warcPaths.length > 0) {
