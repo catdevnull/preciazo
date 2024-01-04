f089ff5047953405df6658881bf46b0e2c8e5b54
Nulo <git@nulo.in>
2024-01-04 19:45:56 -0300

    arreglar types productpreview

--- sitio/src/lib/ProductPreview.svelte
+++ sitio/src/lib/ProductPreview.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-  export let product: { ean: string; name: string; imageUrl: string };
+  export let product: { ean: string; name: string; imageUrl?: string | null };
 </script>
 
 <a href={`/ean/${product.ean}`} class="flex">
-  <img src={product.imageUrl} alt={product.name} class="max-h-48" />
+  {#if product.imageUrl}
+    <img src={product.imageUrl} alt={product.name} class="max-h-48" />
+  {/if}
   <p class="text-xl">{product.name}</p>
 </a>
