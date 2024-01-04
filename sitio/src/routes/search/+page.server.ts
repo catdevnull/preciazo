e890d5f63b5640d37c067236ef7b1b4f057899b2
Nulo <git@nulo.in>
2024-01-04 18:47:24 -0300

    arreglar busqueda para queries extrañas

--- sitio/src/routes/search/+page.server.ts
+++ sitio/src/routes/search/+page.server.ts
@@ -1,19 +1,17 @@
 import { error } from "@sveltejs/kit";
-import { eq, max, sql } from "drizzle-orm";
+import { sql } from "drizzle-orm";
 import type { PageServerLoad } from "./$types";
-import { db, schema } from "$lib/server/db";
-const { precios } = schema;
+import { db } from "$lib/server/db";
 
 export const load: PageServerLoad = async ({ url }) => {
   const query = url.searchParams.get("q");
   let results: null | { ean: string; name: string; imageUrl: string }[] = null;
   if (query) {
-    results = db.all(
-      sql`select p.ean, p.name, p.image_url as imageUrl from precios_fts f
+    const sqlQuery = sql`select p.ean, p.name, p.image_url as imageUrl from precios_fts f
       join precios p on p.ean = f.ean
-      where f.name match ${query}
-      group by p.ean;`,
-    );
+      where f.name match ${`"${query}"`}
+      group by p.ean;`;
+    results = db.all(sqlQuery);
   }
 
   return { query, results };
