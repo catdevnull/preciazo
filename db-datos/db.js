e97b35571533c1d35aeae3a333eb8d75f0b7ec0b
Nulo <git@nulo.in>
2024-01-14 10:47:13 -0300

    no migrar al correr sitio
    voy a tener que resolver esto de alguna otra manera en algún momento

--- db-datos/db.js
+++ db-datos/db.js
@@ -2,10 +2,7 @@
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import { DB_PATH } from "./drizzle.config.js";
-import { migrateDb } from "./migrate.js";
 import * as schema from "./schema.js";
-
-migrateDb();
 
 export const sqlite = new Database(DB_PATH);
 export const db = drizzle(sqlite, { schema });
