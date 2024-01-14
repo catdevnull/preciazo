1214aa5fbe1600e91e7760f27f3119c34782547b
Nulo <git@nulo.in>
2024-01-14 10:18:48 -0300

    migrar BD cuando se corre el sitio

--- sitio/src/lib/server/db.ts
+++ sitio/src/lib/server/db.ts
@@ -1,10 +1,2 @@
-import Database from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import * as schema from "db-datos/schema.js";
-import { env } from "$env/dynamic/private";
-
-const sqlite = new Database(env.DB_PATH ?? "../scraper/sqlite.db");
-const db = drizzle(sqlite, { schema });
-
-export { db };
+export { db } from "db-datos/db.js";
 export * as schema from "db-datos/schema.js";
