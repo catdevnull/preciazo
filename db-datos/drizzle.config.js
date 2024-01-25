fb3518e1982643334e2cec00a6191674785b67cb
Nulo <git@nulo.in>
2024-01-25 18:11:17 -0300

    arreglar drizzle.config

--- db-datos/drizzle.config.js
+++ db-datos/drizzle.config.js
@@ -2,8 +2,9 @@ export const DB_PATH = process.env.DB_PATH ?? "../sqlite.db";
 
 /** @type { import("drizzle-kit").Config } */
 export default {
-  schema: "./schema.ts",
+  schema: "./schema.js",
   out: "./drizzle",
+  driver: "better-sqlite",
   dbCredentials: {
     url: DB_PATH,
   },
