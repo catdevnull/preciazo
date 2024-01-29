4669e6087fb7dbda5e02eedecf57861151b512b5
Nulo <git@nulo.in>
2024-01-29 12:47:09 -0300

    arreglar migracion cli

---
+++ db-datos/migrate-cli.js
@@ -0,0 +1,3 @@
+import { getDb } from "./db.js";
+
+getDb();
--- db-datos/migrate.js
+++ db-datos/migrate.js
@@ -2,12 +2,17 @@
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import * as schema from "./schema.js";
 import { sql } from "drizzle-orm";
+import { existsSync } from "node:fs";
+import { join } from "node:path";
 
 /**
  * @param {import("drizzle-orm/better-sqlite3").BetterSQLite3Database<schema>} db
  */
 export function migrateDb(db) {
-  migrate(db, { migrationsFolder: "node_modules/db-datos/drizzle" });
+  let path = "drizzle/";
+  if (!existsSync(join(path, "meta/_journal.json")))
+    path = "node_modules/db-datos/drizzle";
+  migrate(db, { migrationsFolder: path });
   db.run(sql`pragma journal_mode = WAL;`);
   db.run(sql`PRAGMA synchronous = NORMAL;`);
 }
