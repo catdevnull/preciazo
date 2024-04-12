38b6b62aba5a3c3a7fc07f5fb918f7315c1bb03e
Nulo <git@nulo.in>
2024-04-12 20:32:35 -0300

    cositas en la BD

--- db-datos/db.js
+++ db-datos/db.js
@@ -10,6 +10,12 @@ let db = null;
 export function getDb() {
   if (db) return db;
   const sqlite = new Database(DB_PATH);
+  sqlite.exec(`PRAGMA journal_mode = WAL;`);
+  sqlite.exec(`PRAGMA busy_timeout = 15000;`);
+  sqlite.exec(`PRAGMA synchronous = NORMAL;`);
+  sqlite.exec(`PRAGMA cache_size = 1000000000;`);
+  sqlite.exec(`PRAGMA foreign_keys = true;`);
+  sqlite.exec(`PRAGMA temp_store = memory;`);
   db = drizzle(sqlite, { schema, logger: true });
   migrateDb(db);
   return db;
