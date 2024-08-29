91c7087bdc18f01554ed9e8a1076e51fd32569e5
Nulo <git@nulo.in>
2024-08-28 21:12:34 -0300

    retry all errors

--- sepa-precios-archiver/index.ts
+++ sepa-precios-archiver/index.ts
@@ -150,7 +150,7 @@ for (const resource of datasetInfo.result.resources) {
     console.info(dir);
     try {
       const zip = join(dir, "zip");
-      await $`curl --retry 8 --retry-delay 5 -L -o ${zip} ${resource.url}`;
+      await $`curl --retry 8 --retry-delay 5 --retry-all-errors -L -o ${zip} ${resource.url}`;
       await $`unzip ${zip} -d ${dir}`;
       await rm(zip);
 
