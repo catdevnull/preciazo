2956e7eede9e4032b459245563eb6da5ca30a7be
Nulo <git@nulo.in>
2024-09-14 12:08:57 -0300

    umami

--- sepa/sitio2/src/routes/+layout.svelte
+++ sepa/sitio2/src/routes/+layout.svelte
@@ -6,6 +6,16 @@
 	export let data;
 </script>
 
+<svelte:head>
+	{#if import.meta.env.PROD}
+		<script
+			defer
+			src="https://umami.experimentos.nulo.ar/script.js"
+			data-website-id="3d9215d1-f15c-4797-abff-d621da254930"
+		></script>
+	{/if}
+</svelte:head>
+
 <!-- {#key data.pathname}
 	<div
 		in:fly={{ easing: cubicOut, x: 10, duration: 150, delay: 300 }}
