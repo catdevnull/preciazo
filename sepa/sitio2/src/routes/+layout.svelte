b05776a736837ac91615e10aa3a56c3a3a5783ea
Nulo <git@nulo.in>
2024-09-14 14:00:16 -0300

    meta en index.html

--- sepa/sitio2/src/routes/+layout.svelte
+++ sepa/sitio2/src/routes/+layout.svelte
@@ -14,31 +14,6 @@
 			data-website-id="3d9215d1-f15c-4797-abff-d621da254930"
 		></script>
 	{/if}
-
-	<title>Preciazo</title>
-	<meta
-		name="description"
-		content="Busca precios de productos en distintas cadenas de supermercados en Argentina."
-	/>
-	<meta content="index, follow" name="robots" />
-	<meta property="og:title" content="Preciazo" />
-	<meta
-		property="og:description"
-		content="Busca precios de productos en distintas cadenas de supermercados en Argentina."
-	/>
-	<meta property="og:image" content="https://preciazo.nulo.in/favicon.png" />
-	<meta property="og:url" content={`https://preciazo.nulo.in${data.pathname}`} />
-	<meta property="og:type" content="website" />
-
-	<meta name="twitter:card" content="summary" />
-	<meta name="twitter:site" content="@esoesnulo" />
-	<meta name="twitter:creator" content="@esoesnulo" />
-	<meta name="twitter:title" content="Preciazo" />
-	<meta
-		name="twitter:description"
-		content="Busca precios de productos en distintas cadenas de supermercados en Argentina."
-	/>
-	<meta name="twitter:image" content="https://preciazo.nulo.in/favicon.png" />
 </svelte:head>
 
 <!-- {#key data.pathname}
