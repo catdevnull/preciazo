9aff44b3d94dcde07ad2ebc53765073abd5c9340
Nulo <git@nulo.in>
2024-09-14 17:58:13 -0300

    malvinas
    fixes las malvinas son argentinas #48

--- sepa/sitio2/src/lib/components/Map.svelte
+++ sepa/sitio2/src/lib/components/Map.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onDestroy, onMount, tick } from 'svelte';
 	import 'leaflet/dist/leaflet.css';
+	import style from './map_style.json';
 	import type L from 'leaflet';
 
 	let mapEl: HTMLDivElement;
@@ -8,27 +9,26 @@
 	let map: L.Map | undefined;
 
 	onMount(async () => {
-		const L = await import('leaflet');
+		window.L = await import('leaflet');
+		const L2 = await import('maplibre-gl');
+		const L3 = await import('@maplibre/maplibre-gl-leaflet');
 		// Set up initial map center and zoom level
-		map = L.map(mapEl, {
+		map = window.L.map(mapEl, {
 			center: [-34.599722222222, -58.381944444444], // EDIT latitude, longitude to re-center map
 			zoom: 9, // EDIT from 1 to 18 -- decrease to zoom out, increase to zoom in
 			scrollWheelZoom: true // Changed to true to enable zoom with scrollwheel
 			// tap: false
 		});
 
-		/* Control panel to display map layers */
-		// var controlLayers = L.control.layers( null, null, {
-		//   position: "topright",
-		//   collapsed: false
-		// }).addTo(map);
+		window.L.maplibreGL({
+			style: style as any,
+			attribution:
+				'&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
+		} as any).addTo(map);
 
 		// display Carto basemap tiles with light features and labels
-		L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-			attribution:
-				'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
-		}).addTo(map);
-		mapMap(map, L);
+		// L.tileLayer.provider('Stadia.AlidadeSmoothBackground').addTo(map);
+		mapMap(map, window.L);
 	});
 
 	onDestroy(() => {
